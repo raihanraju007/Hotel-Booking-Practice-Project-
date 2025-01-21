d0c42ddfc6fd1f6e76d09b0e3601ae7916f38154
raihanraju007 <raihanraju007@gmail.com>
2025-01-21 17:01:56 +0600

    Hotel Entity,Service and Repository COmplate

---
+++ src/main/java/com/raju/hotelbooking/repository/HotelRepository.java
@@ -0,0 +1,17 @@
+package com.raju.hotelbooking.repository;
+
+import com.raju.hotelbooking.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HotelRepository extends JpaRepository<Hotel, Integer> {
+
+
+    @Query("Select h from Hotel h where h.location.name= :locationName")
+    List<Hotel> findHotelByLocationName(@Param("locationName")String locationName);
+}
---
+++ src/main/java/com/raju/hotelbooking/service/HotelService.java
@@ -0,0 +1,97 @@
+package com.raju.hotelbooking.service;
+
+import com.raju.hotelbooking.entity.Hotel;
+import com.raju.hotelbooking.entity.Location;
+import com.raju.hotelbooking.repository.HotelRepository;
+import com.raju.hotelbooking.repository.LocationRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class HotelService {
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @Value("src/main/resources/static/images")
+    private String uploadDir;
+
+    public List<Hotel> getAllHotels(){
+
+        return hotelRepository.findAll();
+    }
+
+    public void saveHotel(Hotel hotel, MultipartFile imageFile) throws IOException {
+
+        if (imageFile!=null && !imageFile.isEmpty()){
+            String imageFileName = saveImage(imageFile,hotel);
+            hotel.setImage(imageFileName);
+        }
+        Location location = locationRepository.findById(hotel.getLocation().getId())
+                .orElseThrow(()->new EntityNotFoundException("Location not found with Id: "+ hotel.getLocation().getId()));
+
+        hotel.setLocation(location);
+        hotelRepository.save(hotel);
+
+    }
+
+    public Hotel findHotelById(int id){
+        return hotelRepository.findById(id)
+                .orElseThrow(()->new EntityNotFoundException("Hotel not found with Id: " +id));
+    }
+
+    public void updateHotel(int id,Hotel hotel, MultipartFile image) throws IOException {
+
+        Hotel existingHotel = hotelRepository.findById(id)
+                .orElseThrow(()->new EntityNotFoundException("Hotel not found with Id: " +id));
+
+        existingHotel.setName(hotel.getName());
+        existingHotel.setAddress((hotel.getAddress()));
+        existingHotel.setRating(hotel.getRating());
+        existingHotel.setMaximumPrice(hotel.getMaximumPrice());
+        existingHotel.setMinimumPrice(hotel.getMaximumPrice());
+
+        // update location
+        Location location = locationRepository.findById(hotel.getLocation().getId())
+                .orElseThrow(()->new EntityNotFoundException("Location not found with Id: "+ hotel.getLocation().getId()));
+
+        existingHotel.setLocation(location);
+
+        // update image
+        if (image !=null && !image.isEmpty()){
+            String fileName = saveImage(image,existingHotel);
+            existingHotel.setImage(fileName);
+        }
+
+    }
+
+    public List<Hotel> findHotelByLocationName(String locationName){
+        return hotelRepository.findHotelByLocationName(locationName);
+    }
+
+
+
+    private String saveImage(MultipartFile file, Hotel hotel) throws IOException {
+        Path uploadPath = Paths.get(uploadDir+"/hotels");
+        if(! Files.exists(uploadPath)){
+            Files.createDirectories(uploadPath);
+        }
+        String fileName = hotel.getName()+"_"+ UUID.randomUUID().toString();
+        Path filePath=uploadPath.resolve(fileName);
+        Files.copy(file.getInputStream(),filePath);
+
+        return  fileName;
+    }
+}
